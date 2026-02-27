8689867f9fb235d5a0a09944dbc1360d1621ea21
Aluno FIC <gisleneperuzzo@gmail.com>
2026-02-26 21:01:24 -0300

    userdetailsservice

--- src/main/java/Senai/Ipiranga/com/blogPessoal/Seguranca/UserDetailsServiceImpl.java
+++ src/main/java/Senai/Ipiranga/com/blogPessoal/Seguranca/UserDetailsServiceImpl.java
@@ -1,5 +1,26 @@
 package Senai.Ipiranga.com.blogPessoal.Seguranca;
 
-public class UserDetailsServiceImpl {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import Senai.Ipiranga.com.blogPessoal.Model.Usuario;
+import Senai.Ipiranga.com.blogPessoal.Repository.UsuarioRepository;
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private 	UsuarioRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException{
+		Optional<Usuario> user = userRepository.findByUsuario(userName);
+		user.orElseThrow(()-> new UsernameNotFoundException(userName + "notfound"));
+		return user.map(UserDetailsImpl::new).get();
+	}
+	
 }
